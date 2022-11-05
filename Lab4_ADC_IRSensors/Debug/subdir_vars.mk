c7df94ff36729b4e839bcf4d665948f257eb1b47
Ning <97502167+dcxjn@users.noreply.github.com>
2022-11-06 03:06:04 +0800

    Add files via upload

---
+++ Lab4_ADC_IRSensors/Debug/subdir_vars.mk
@@ -0,0 +1,89 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+D:/Y2S1/SC2107\ Lab/inc/ADC14.c \
+D:/Y2S1/SC2107\ Lab/inc/Clock.c \
+D:/Y2S1/SC2107\ Lab/inc/CortexM.c \
+D:/Y2S1/SC2107\ Lab/inc/IRDistance.c \
+D:/Y2S1/SC2107\ Lab/inc/LPF.c \
+../Lab4_ADCmain.c \
+D:/Y2S1/SC2107\ Lab/inc/LaunchPad.c \
+D:/Y2S1/SC2107\ Lab/inc/TimerA1.c \
+D:/Y2S1/SC2107\ Lab/inc/UART0.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./ADC14.d \
+./Clock.d \
+./CortexM.d \
+./IRDistance.d \
+./LPF.d \
+./Lab4_ADCmain.d \
+./LaunchPad.d \
+./TimerA1.d \
+./UART0.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./ADC14.obj \
+./Clock.obj \
+./CortexM.obj \
+./IRDistance.obj \
+./LPF.obj \
+./Lab4_ADCmain.obj \
+./LaunchPad.obj \
+./TimerA1.obj \
+./UART0.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"ADC14.obj" \
+"Clock.obj" \
+"CortexM.obj" \
+"IRDistance.obj" \
+"LPF.obj" \
+"Lab4_ADCmain.obj" \
+"LaunchPad.obj" \
+"TimerA1.obj" \
+"UART0.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"ADC14.d" \
+"Clock.d" \
+"CortexM.d" \
+"IRDistance.d" \
+"LPF.d" \
+"Lab4_ADCmain.d" \
+"LaunchPad.d" \
+"TimerA1.d" \
+"UART0.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"D:/Y2S1/SC2107 Lab/inc/ADC14.c" \
+"D:/Y2S1/SC2107 Lab/inc/Clock.c" \
+"D:/Y2S1/SC2107 Lab/inc/CortexM.c" \
+"D:/Y2S1/SC2107 Lab/inc/IRDistance.c" \
+"D:/Y2S1/SC2107 Lab/inc/LPF.c" \
+"../Lab4_ADCmain.c" \
+"D:/Y2S1/SC2107 Lab/inc/LaunchPad.c" \
+"D:/Y2S1/SC2107 Lab/inc/TimerA1.c" \
+"D:/Y2S1/SC2107 Lab/inc/UART0.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
---
+++ Lab4_TimerCapture_Tach/Debug/subdir_vars.mk
@@ -0,0 +1,101 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432p401r.cmd 
+
+C_SRCS += \
+D:/Y2S1/SC2107\ Lab/inc/Bump.c \
+D:/Y2S1/SC2107\ Lab/inc/Clock.c \
+D:/Y2S1/SC2107\ Lab/inc/CortexM.c \
+../Lab4_Tachmain.c \
+D:/Y2S1/SC2107\ Lab/inc/LaunchPad.c \
+D:/Y2S1/SC2107\ Lab/inc/Motor.c \
+D:/Y2S1/SC2107\ Lab/inc/PWM.c \
+D:/Y2S1/SC2107\ Lab/inc/SysTick.c \
+D:/Y2S1/SC2107\ Lab/inc/TA3InputCapture.c \
+D:/Y2S1/SC2107\ Lab/inc/TExaS.c \
+D:/Y2S1/SC2107\ Lab/inc/UART0.c \
+../startup_msp432p401r_ccs.c \
+../system_msp432p401r.c 
+
+C_DEPS += \
+./Bump.d \
+./Clock.d \
+./CortexM.d \
+./Lab4_Tachmain.d \
+./LaunchPad.d \
+./Motor.d \
+./PWM.d \
+./SysTick.d \
+./TA3InputCapture.d \
+./TExaS.d \
+./UART0.d \
+./startup_msp432p401r_ccs.d \
+./system_msp432p401r.d 
+
+OBJS += \
+./Bump.obj \
+./Clock.obj \
+./CortexM.obj \
+./Lab4_Tachmain.obj \
+./LaunchPad.obj \
+./Motor.obj \
+./PWM.obj \
+./SysTick.obj \
+./TA3InputCapture.obj \
+./TExaS.obj \
+./UART0.obj \
+./startup_msp432p401r_ccs.obj \
+./system_msp432p401r.obj 
+
+OBJS__QUOTED += \
+"Bump.obj" \
+"Clock.obj" \
+"CortexM.obj" \
+"Lab4_Tachmain.obj" \
+"LaunchPad.obj" \
+"Motor.obj" \
+"PWM.obj" \
+"SysTick.obj" \
+"TA3InputCapture.obj" \
+"TExaS.obj" \
+"UART0.obj" \
+"startup_msp432p401r_ccs.obj" \
+"system_msp432p401r.obj" 
+
+C_DEPS__QUOTED += \
+"Bump.d" \
+"Clock.d" \
+"CortexM.d" \
+"Lab4_Tachmain.d" \
+"LaunchPad.d" \
+"Motor.d" \
+"PWM.d" \
+"SysTick.d" \
+"TA3InputCapture.d" \
+"TExaS.d" \
+"UART0.d" \
+"startup_msp432p401r_ccs.d" \
+"system_msp432p401r.d" 
+
+C_SRCS__QUOTED += \
+"D:/Y2S1/SC2107 Lab/inc/Bump.c" \
+"D:/Y2S1/SC2107 Lab/inc/Clock.c" \
+"D:/Y2S1/SC2107 Lab/inc/CortexM.c" \
+"../Lab4_Tachmain.c" \
+"D:/Y2S1/SC2107 Lab/inc/LaunchPad.c" \
+"D:/Y2S1/SC2107 Lab/inc/Motor.c" \
+"D:/Y2S1/SC2107 Lab/inc/PWM.c" \
+"D:/Y2S1/SC2107 Lab/inc/SysTick.c" \
+"D:/Y2S1/SC2107 Lab/inc/TA3InputCapture.c" \
+"D:/Y2S1/SC2107 Lab/inc/TExaS.c" \
+"D:/Y2S1/SC2107 Lab/inc/UART0.c" \
+"../startup_msp432p401r_ccs.c" \
+"../system_msp432p401r.c" 
+
+
