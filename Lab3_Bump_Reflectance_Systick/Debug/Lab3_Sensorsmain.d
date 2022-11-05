0dbfc46995de2835932e96fd567cd3fae27b8f26
Ning <97502167+dcxjn@users.noreply.github.com>
2022-11-06 03:02:52 +0800

    Add files via upload

---
+++ Lab3_Bump_Reflectance_Systick/Debug/Lab3_Sensorsmain.d
@@ -0,0 +1,81 @@
+# FIXED
+
+Lab3_Sensorsmain.obj: ../Lab3_Sensorsmain.c
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp_compatibility.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r_classic.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+Lab3_Sensorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/system_msp432p401r.h
+Lab3_Sensorsmain.obj: ../../inc/bump.h
+Lab3_Sensorsmain.obj: ../../inc/Reflectance.h
+Lab3_Sensorsmain.obj: ../../inc/Clock.h
+Lab3_Sensorsmain.obj: ../../inc/SysTickInts.h
+Lab3_Sensorsmain.obj: ../../inc/CortexM.h
+Lab3_Sensorsmain.obj: ../../inc/LaunchPad.h
+Lab3_Sensorsmain.obj: ../../inc/TExaS.h
+
+../Lab3_Sensorsmain.c:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp_compatibility.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r_classic.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/core_cm4.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/system_msp432p401r.h:
+
+../../inc/bump.h:
+
+../../inc/Reflectance.h:
+
+../../inc/Clock.h:
+
+../../inc/SysTickInts.h:
+
+../../inc/CortexM.h:
+
+../../inc/LaunchPad.h:
+
+../../inc/TExaS.h:
+
---
+++ Lab3_TimerCompare_Motor/Debug/CortexM.d
@@ -0,0 +1,7 @@
+# FIXED
+
+CortexM.obj: D:/Y2S1/SC2107\ Lab/inc/CortexM.c
+CortexM.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+
+D:/Y2S1/SC2107\ Lab/inc/CortexM.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
