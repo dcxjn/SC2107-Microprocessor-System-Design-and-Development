64722dcb2754ed94b8474e76b9a239e32f8d1bcd
Ning <97502167+dcxjn@users.noreply.github.com>
2022-11-06 03:04:30 +0800

    Add files via upload

---
+++ Lab3ref_SimpleMotors/Debug/MotorSimple.d
@@ -0,0 +1,66 @@
+# FIXED
+
+MotorSimple.obj: D:/Y2S1/SC2107\ Lab/inc/MotorSimple.c
+MotorSimple.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp_compatibility.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r_classic.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+MotorSimple.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/system_msp432p401r.h
+MotorSimple.obj: D:/Y2S1/SC2107\ Lab/inc/SysTick.h
+MotorSimple.obj: D:/Y2S1/SC2107\ Lab/inc/Bump.h
+
+D:/Y2S1/SC2107\ Lab/inc/MotorSimple.c:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp_compatibility.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r_classic.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/core_cm4.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/system_msp432p401r.h:
+
+D:/Y2S1/SC2107\ Lab/inc/SysTick.h:
+
+D:/Y2S1/SC2107\ Lab/inc/Bump.h:
+
---
+++ Lab3ref_SimpleMotors/Debug/Simple_Motorsmain.d
@@ -0,0 +1,75 @@
+# FIXED
+
+Simple_Motorsmain.obj: ../Simple_Motorsmain.c
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp_compatibility.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r_classic.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+Simple_Motorsmain.obj: C:/ti/ccs1200/ccs/ccs_base/arm/include/system_msp432p401r.h
+Simple_Motorsmain.obj: ../../inc/bump.h
+Simple_Motorsmain.obj: ../../inc/Clock.h
+Simple_Motorsmain.obj: ../../inc/SysTick.h
+Simple_Motorsmain.obj: ../../inc/LaunchPad.h
+Simple_Motorsmain.obj: ../../inc/MotorSimple.h
+
+../Simple_Motorsmain.c:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_ti_config.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/linkage.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/_stdint40.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_types.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-arm_20.2.6.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp_compatibility.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/msp432p401r_classic.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/core_cm4.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h:
+
+C:/ti/ccs1200/ccs/ccs_base/arm/include/system_msp432p401r.h:
+
+../../inc/bump.h:
+
+../../inc/Clock.h:
+
+../../inc/SysTick.h:
+
+../../inc/LaunchPad.h:
+
+../../inc/MotorSimple.h:
+
